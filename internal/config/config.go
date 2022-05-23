f3859f34e0bc87fd17395e4f38881f5660eeaf6a
Ivan Polennikov <lerner1796@gmail.com>
2022-05-23 22:43:41 +0300

    Increment 7

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,42 +2,50 @@ package config
 
 import (
 	"flag"
+	"fmt"
 	"log"
-	"os"
 	"sync"
+
+	"github.com/caarlos0/env/v6"
 )
 
 type Config struct {
-	ServerAddress   string
-	BaseURL         string
-	FileStoragePath string
-}
-
-func getEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
-	}
-	return fallback
+	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
+	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH"`
 }
 
 var instance *Config
 
+func (c *Config) init() {
+	if err := env.Parse(c); err != nil {
+		fmt.Printf("Cannot parse env vars %v\n", err)
+	}
+	serverAddressPtr := flag.String("a", "", "")
+	baseURLPtr := flag.String("b", "", "")
+	fileStoragePathPtr := flag.String("f", "", "")
+	flag.Parse()
+
+	if *serverAddressPtr != "" {
+		c.ServerAddress = *serverAddressPtr
+	}
+
+	if *baseURLPtr != "" {
+		c.BaseURL = *baseURLPtr
+	}
+
+	if *fileStoragePathPtr != "" {
+		c.FileStoragePath = *fileStoragePathPtr
+	}
+}
+
 var once sync.Once
 
 func GetConfig() *Config {
-
+	log.Println("Load config...")
 	once.Do(func() {
-		log.Println("Load config...")
-		addressPtr := flag.String("a", getEnv("SERVER_ADDRESS", "localhost:8080"), "SERVER_ADDRESS")
-		baseURLPtr := flag.String("b", getEnv("BASE_URL", "localhost:8080"), "BASE_URL")
-		fileStoragePathPtr := flag.String("f", getEnv("FILE_STORAGE_PATH", ""), "FILE_STORAGE_PATH")
-		flag.Parse()
-
-		instance = &Config{
-			ServerAddress:   *addressPtr,
-			BaseURL:         *baseURLPtr,
-			FileStoragePath: *fileStoragePathPtr,
-		}
+		instance = new(Config)
+		instance.init()
 	})
 	log.Println("Successfully load config from env variables")
 	return instance
