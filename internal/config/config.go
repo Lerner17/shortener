1bf2b158d864e24be2139ed182d0c79004e343ae
Ivan Polennikov <lerner1796@gmail.com>
2022-05-23 22:18:08 +0300

    Increment 7

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,16 +1,23 @@
 package config
 
 import (
+	"flag"
 	"log"
+	"os"
 	"sync"
-
-	"github.com/alexflint/go-arg"
 )
 
 type Config struct {
-	ServerAddress   string `arg:"-a,env:SERVER_ADDRESS" default:"localhost:8080"`
-	BaseURL         string `arg:"-b,env:BASE_URL" default:"localhost:8080"`
-	FileStoragePath string `arg:"-f,env:FILE_STORAGE_PATH" default:""`
+	ServerAddress   string
+	BaseURL         string
+	FileStoragePath string
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
 }
 
 var instance *Config
@@ -18,10 +25,19 @@ var instance *Config
 var once sync.Once
 
 func GetConfig() *Config {
+
 	once.Do(func() {
 		log.Println("Load config...")
-		instance = &Config{}
-		arg.MustParse(instance)
+		addressPtr := flag.String("a", getEnv("SERVER_ADDRESS", "localhost:8080"), "SERVER_ADDRESS")
+		baseURLPtr := flag.String("b", getEnv("BASE_URL", "localhost:8080"), "BASE_URL")
+		fileStoragePathPtr := flag.String("f", getEnv("FILE_STORAGE_PATH", ""), "FILE_STORAGE_PATH")
+		flag.Parse()
+
+		instance = &Config{
+			ServerAddress:   *addressPtr,
+			BaseURL:         *baseURLPtr,
+			FileStoragePath: *fileStoragePathPtr,
+		}
 	})
 	log.Println("Successfully load config from env variables")
 	return instance
