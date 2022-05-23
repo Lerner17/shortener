5cd5c8c74836b976871ae3f9a8f38fdc18c37a30
Ivan Polennikov <lerner1796@gmail.com>
2022-05-23 21:20:28 +0300

    Increment 7

--- go.mod
+++ go.mod
@@ -9,7 +9,9 @@ require (
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/alexflint/go-arg v1.4.3 // indirect
+	github.com/alexflint/go-scalar v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,17 +1,16 @@
 package config
 
 import (
-	"flag"
 	"log"
 	"sync"
 
-	"github.com/caarlos0/env/v6"
+	"github.com/alexflint/go-arg"
 )
 
 type Config struct {
-	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
-	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH"`
+	ServerAddress   string `arg:"-a,env:SERVER_ADDRESS" default:"localhost:8080"`
+	BaseURL         string `arg:"-b,env:BASE_URL" default:"http://localhost:8080"`
+	FileStoragePath string `arg:"-f,env:FILE_STORAGE_PATH" default:""`
 }
 
 var instance *Config
@@ -21,22 +20,20 @@ var once sync.Once
 func GetConfig() *Config {
 	once.Do(func() {
 		log.Println("Load config...")
-		addressPtr := flag.String("a", "", "HOST")
-		baseURLPtr := flag.String("b", "", "BASE_URL")
-		fileStoragePathPtr := flag.String("f", "", "FILE_STORAGE_PATH")
-		flag.Parse()
-		if *addressPtr != "" || *baseURLPtr != "" {
-			instance = &Config{
-				ServerAddress:   *addressPtr,
-				BaseURL:         *baseURLPtr,
-				FileStoragePath: *fileStoragePathPtr,
-			}
-			return
-		}
+		// addressPtr := flag.String("a", "", "SERVER_ADDRESS")
+		// baseURLPtr := flag.String("b", "", "BASE_URL")
+		// fileStoragePathPtr := flag.String("f", "", "FILE_STORAGE_PATH")
+		// flag.Parse()
+		// if *addressPtr != "" || *baseURLPtr != "" {
+		// 	instance = &Config{
+		// 		ServerAddress:   *addressPtr,
+		// 		BaseURL:         *baseURLPtr,
+		// 		FileStoragePath: *fileStoragePathPtr,
+		// 	}
+		// 	return
+		// }
 		instance = &Config{}
-		if err := env.Parse(instance); err != nil {
-			log.Fatalf("Cannot parse env variables: %v", err)
-		}
+		arg.MustParse(instance)
 	})
 	log.Println("Successfully load config from env variables")
 	return instance
