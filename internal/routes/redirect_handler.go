34966bc4c802c9070b66ca9f8d18624f4c02fe51
Ivan Lerner <lerner1796@gmail.com>
2022-09-07 17:39:34 +0300

    Increment 9: Add session middleware

--- internal/routes/redirect_handler.go
+++ internal/routes/redirect_handler.go
@@ -3,7 +3,9 @@ package routes
 import (
 	"net/http"
 
+	"github.com/Lerner17/shortener/internal/logger"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 )
 
 type URLGetter interface {
@@ -13,13 +15,18 @@ type URLGetter interface {
 func RedirectHandler(db URLGetter) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		urlID := chi.URLParam(r, "urlID")
+		logger.Info("URL ID:", zap.String("urlID", urlID))
 		ctx := r.Context()
 		session, ok := ctx.Value("ctxSession").(string)
+		logger.Info("Session:", zap.String("session", session))
 		if !ok {
 			http.Error(w, http.StatusText(422), 422)
 			return
 		}
-		if fullURL, ok := db.GetURL(session, urlID); ok {
+		fullURL, ok := db.GetURL(session, urlID)
+		logger.Info("Get from DB status", zap.Bool("ok", ok))
+		logger.Info("Value from database", zap.String("value", fullURL))
+		if ok {
 			w.Header().Set("Content-Type", "plain/text")
 			http.Redirect(w, r, fullURL, http.StatusTemporaryRedirect)
 		} else {
