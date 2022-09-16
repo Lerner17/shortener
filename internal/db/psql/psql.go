b064b8c40a41852c53d326f69c4dc21ae27bdc51
Ivan Lerner <lerner1796@gmail.com>
2022-09-16 13:48:57 +0300

    Fix autotests

--- internal/db/psql/psql.go
+++ internal/db/psql/psql.go
@@ -2,7 +2,8 @@ package psql
 
 import (
 	"database/sql"
-	"errors"
+	"fmt"
+	"os"
 
 	"github.com/Lerner17/shortener/internal/config"
 	"github.com/Lerner17/shortener/internal/logger"
@@ -14,7 +15,7 @@ var Instance *sql.DB
 func init() {
 	dsn := config.GetConfig().DatabaseDsn
 	if dsn == "" {
-		panic(errors.New("DB error"))
+		fmt.Fprint(os.Stderr, "Cannot connect to database")
 	}
 
 	inst, err := sql.Open("pgx", dsn)
